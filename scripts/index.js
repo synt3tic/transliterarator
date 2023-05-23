a95463c97e8ef7b172f6c82c94de18d3dbcb6827
Vlad Sapozhnikov <mel04nax@gmail.com>
2023-05-23 17:15:23 +0300

    micro fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const button = document.querySelector('.input-box__button')
-const ruField = document.querySelector('.result-box__input')
-const enField = document.querySelector('.result-box__output')
+const ruField = document.querySelector('#input')
+const enField = document.querySelector('#output')
 const alphabet = {
     'а': 'a',
     'б': 'b',
@@ -35,64 +35,33 @@ const alphabet = {
     'э': 'e',
     'ю': 'yu',
     'я': 'ya',
-    ' ': ' ',
-    'А': 'A',
-    'Б': 'B',
-    'В': 'V',
-    'Г': 'G',
-    'Д': 'D',
-    'Е': 'E',
-    'Ё': 'Yo',
-    'Ж': 'Zh',
-    'З': 'Z',
-    'И': 'I',
-    'Й': 'Y',
-    'К': 'K',
-    'Л': 'L',
-    'М': 'M',
-    'Н': 'N',
-    'О': 'O',
-    'П': 'P',
-    'Р': 'R',
-    'С': 'S',
-    'Т': 'T',
-    'У': 'U',
-    'Ф': 'F',
-    'Х': 'H',
-    'Ц': 'C',
-    'Ч': 'Ch',
-    'Ш': 'Sh',
-    'Щ': 'Shc',
-    'Ъ': '"',
-    'Ы': 'Y',
-    'Ь': "'",
-    'Э': 'E',
-    'Ю': 'Yu',
-    'Я': 'Ya',
 }
 
+
 function translit(str){
-    let strArray = str.split('')
-    return strArray.map((el) => {
-        if(alphabet.hasOwnProperty(el)){
-            return el = alphabet[el]
-        }else{
+    return str.split('').map((el) => {
+        if (!!el.match(/[А-Я]/)) {
+            return alphabet[el.toLowerCase()].toUpperCase()
+        } else {
+            if (alphabet[el]) {
+            return alphabet[el]
+        } else {
             return el
+        }
         }
     }).join('')
 }
 
 button.addEventListener('click', () => {
-    const inputText = document.querySelector('.input-box__input').value
+    let inputText = document.querySelector('.input-box__input')
     let ruDiv = document.createElement('div')
     let enDiv = document.createElement('div')
-
     ruField.appendChild(ruDiv)
     enField.appendChild(enDiv)
 
-    ruDiv.textContent = inputText
-    enDiv.textContent = translit(inputText)
+    ruDiv.textContent = inputText.value
+    enDiv.textContent = translit(inputText.value)
     ruDiv.className = 'new-text'
     enDiv.className = 'new-text'
-    
+    inputText.value = ''
 })
