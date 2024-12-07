0dc3b53c4d40dd679127e5db0fdeda7f804723c9
v.sapozhnikov <v.sapozhnikov@idaproject.com>
2024-12-07 19:19:53 +0300

    refactor(index.js): небольшой рефактор кода

--- scripts/index.js
+++ scripts/index.js
@@ -1,67 +1,44 @@
-const button = document.querySelector('.input-box__button')
-const ruField = document.querySelector('#input')
-const enField = document.querySelector('#output')
+const button = document.querySelector('.input-box__button');
+const ruField = document.querySelector('#input');
+const enField = document.querySelector('#output');
+const inputText = document.querySelector('.input-box__input');
+
 const alphabet = {
-    'а': 'a',
-    'б': 'b',
-    'в': 'v',
-    'г': 'g',
-    'д': 'd',
-    'е': 'e',
-    'ё': 'yo',
-    'ж': 'zh',
-    'з': 'z',
-    'и': 'i',
-    'й': 'y',
-    'к': 'k',
-    'л': 'l',
-    'м': 'm',
-    'н': 'n',
-    'о': 'o',
-    'п': 'p',
-    'р': 'r',
-    'с': 's',
-    'т': 't',
-    'у': 'u',
-    'ф': 'f',
-    'х': 'h',
-    'ц': 'c',
-    'ч': 'ch',
-    'ш': 'sh',
-    'щ': 'shc',
-    'ъ': '"',
-    'ы': 'y',
-    'ь': "'",
-    'э': 'e',
-    'ю': 'yu',
-    'я': 'ya',
-}
+    'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd',
+    'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i',
+    'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n',
+    'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't',
+    'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch',
+    'ш': 'sh', 'щ': 'shc', 'ъ': '"', 'ы': 'y', 'ь': "'",
+    'э': 'e', 'ю': 'yu', 'я': 'ya',
+};
 
 
-function translit(str){
-    return str.split('').map((el) => {
-        if (!!el.match(/[А-Я]/)) {
-            return alphabet[el.toLowerCase()].toUpperCase()
-        } else {
-            if (alphabet[el]) {
-            return alphabet[el]
-        } else {
-            return el
-        }
-        }
+const translit = (str) => {
+    return str.split('').map((char) => {
+        const isUpperCase = char.match(/[А-Я]/);
+        const lowerChar = char.toLowerCase();
+        const translatedChar = alphabet[lowerChar] || char;
+
+        return isUpperCase ? translatedChar.toUpperCase() : translatedChar;
     }).join('')
 }
 
-button.addEventListener('click', () => {
-    let inputText = document.querySelector('.input-box__input')
-    let ruDiv = document.createElement('div')
-    let enDiv = document.createElement('div')
-    ruField.appendChild(ruDiv)
-    enField.appendChild(enDiv)
+const createTextBlock = (container, text) => {
+    const div = document.createElement('div');
+    div.textContent = text;
+    div.className = 'new-text';
+    container.appendChild(div);
+};
 
-    ruDiv.textContent = inputText.value
-    enDiv.textContent = translit(inputText.value)
-    ruDiv.className = 'new-text'
-    enDiv.className = 'new-text'
-    inputText.value = ''
-})
+button.addEventListener('click', () => {
+    const text = inputText.value.trim();
+
+    if (!text) {
+        return;
+    }
+
+    createTextBlock(ruField, text);
+    createTextBlock(enField, translit(text));
+    inputText.value = '';
+});
